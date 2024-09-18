367db5b7deaf6a8d2746eef5e445212dbacb65b4
Joe Thompson <thompsonjoejj@gmail.com>
2024-09-18 22:48:15 +0100

    Updated env variable retrieval mechanism

--- src/lib/stripe/stripe.config.ts
+++ src/lib/stripe/stripe.config.ts
@@ -1,5 +1,6 @@
 import Stripe from 'stripe';
-import { SECRET_STRIPE_API_KEY } from '$env/static/private';
+const stripeSecretKey =
+	process.env.SECRET_STRIPE_API_KEY || import.meta.env.SECRET_STRIPE_API_KEY;
 
-const stripe = new Stripe(SECRET_STRIPE_API_KEY);
+const stripe = new Stripe(stripeSecretKey);
 export default stripe;
