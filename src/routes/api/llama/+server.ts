6d549d46d9514868ad2a8ce66eea98af37828a97
Joe Thompson <thompsonjoejj@gmail.com>
2024-10-12 16:46:18 +0100

    Updated defunked llama api endpoint

--- src/routes/api/llama/+server.ts
+++ src/routes/api/llama/+server.ts
@@ -1,33 +1,127 @@
-import { error } from '@sveltejs/kit';
 // @ts-ignore
 import LlamaAI from 'llamaai';
-
+import { error } from '@sveltejs/kit';
+import type { Message, Model, Image, ChatGPTImage } from '$lib/types';
+import { calculateGptVisionPricing, countTokens } from '$lib/tokenizer';
+import { createApiRequestEntry } from '$lib/db/crud/apiRequest';
+import { retrieveUsersBalance, updateUserBalanceWithDeduction } from '$lib/db/crud/balance';
+import type { Message as MessageEntity } from '@prisma/client';
+import { createMessage } from '$lib/db/crud/message';
+import { InsufficientBalanceError } from '$lib/customErrors';
 import { env } from '$env/dynamic/private';
-const llamaAPI = new LlamaAI(env.VITE_LLAMA_API_KEY);
 
 export async function POST({ request, locals }) {
-	let session = await locals.getSession();
-	if (!session) {
+	let session = await locals.auth();
+	if (!session || !session.user || !session.user.email) {
 		throw error(401, 'Forbidden');
 	}
-	try {
-		const { prompt, model } = await request.json();
-		const apiRequestJson = {
-			model: model,
-			messages: [{ role: 'user', content: prompt }],
-			stream: true
-		};
 
-		const stream = await llamaAPI.run(apiRequestJson);
+	try {
+		const { plainTextPrompt, promptStr, modelStr, imagesStr } = await request.json();
+
+		const plainText: string = JSON.parse(plainTextPrompt);
+
+		const model: Model = JSON.parse(modelStr);
+
+		let messages: Message[] = JSON.parse(promptStr);
+
+		const images: Image[] = JSON.parse(imagesStr);
+
+		let gptImages: ChatGPTImage[] = [];
+
+		const llamaAPI = new LlamaAI(env.VITE_LLAMA_API_KEY);
+
+		const inputGPTCount = await countTokens(messages, model, 'input');
+
+		if (images.length > 0) {
+			const textObject = {
+				type: 'text',
+				text: messages[messages.length - 1].content
+			};
+			gptImages = images.map((image) => ({
+				type: 'image_url',
+				image_url: {
+					url: image.data
+				}
+			}));
+			messages[messages.length - 1].content = [textObject, ...gptImages];
+		}
+
+		let imageCost = 0;
+		let imageTokens = 0;
+		for (const image of images) {
+			const result = calculateGptVisionPricing(image.width, image.height);
+			imageCost += result.price;
+			imageTokens += result.tokens;
+		}
+
+		const inputCost = inputGPTCount.price + imageCost;
+		let balance = await retrieveUsersBalance(Number(session.user.id));
+		if (balance - inputCost <= 0.1) {
+			throw new InsufficientBalanceError();
+		}
+
+		const stream = await llamaAPI.run({
+			model: model,
+			messages: { role: 'user', content: 'What is the weather like in Boston?' },
+			stream: false
+		});
+
+		const chunks: string[] = [];
+		let error: any;
+
 		const readableStream = new ReadableStream({
 			async start(controller) {
-				for await (const chunk of stream) {
-					const content = chunk.choices[0]?.delta?.content || ''; // adjust based on actual response structure
-					if (content) {
-						controller.enqueue(new TextEncoder().encode(`${content}`));
+				try {
+					for await (const chunk of stream) {
+						const content = chunk.choices[0]?.delta?.content || '';
+						if (content) {
+							chunks.push(content);
+							controller.enqueue(new TextEncoder().encode(`${content}`));
+						}
 					}
+					controller.close();
+				} catch (err) {
+					console.error('Error in stream processing: ', err);
+					error = err;
+					// controller.enqueue(new TextEncoder().encode(errorMessage)); // see if this works
+				} finally {
+					if (chunks.length > 0) {
+						const response = chunks.join('');
+						const outputGPTCount = await countTokens(response, model, 'output');
+
+						const message: MessageEntity = await createMessage(
+							plainText,
+							response,
+							images
+							// need to add previous message ids
+						);
+
+						await updateUserBalanceWithDeduction(
+							Number(session.user!.id),
+							outputGPTCount.price + inputCost
+						);
+
+						await createApiRequestEntry(
+							Number(session.user!.id!),
+							'openAI',
+							model.name,
+							inputGPTCount.tokens + imageTokens,
+							inputCost,
+							outputGPTCount.tokens,
+							outputGPTCount.price,
+							inputCost + outputGPTCount.price,
+							message
+						);
+					}
+					if (error) {
+						const errorMessage = JSON.stringify({
+							error: error.error.error.message || 'An unknown error occurred'
+						});
+						controller.enqueue(new TextEncoder().encode(errorMessage));
+					}
+					controller.close();
 				}
-				controller.close();
 			}
 		});
 
@@ -39,6 +133,9 @@ export async function POST({ request, locals }) {
 			}
 		});
 	} catch (err) {
+		if (err instanceof InsufficientBalanceError) {
+			throw error(500, err.message);
+		}
 		console.error('Error:', err);
 		throw error(500, 'An error occurred while processing your request');
 	}
